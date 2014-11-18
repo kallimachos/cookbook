86a0f5e6d4842e164252b15937babb8b1be73b0c
kallimachos <kallimachos@gmail.com>
2014-11-18 12:18:18 +1000

    add extra options to args.py

--- args.py
+++ args.py
@@ -7,8 +7,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="calculate X to the power of Y")
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
+group.add_argument("-v", "--verbose", action="store_true", help="verbose output")
+group.add_argument("-q", "--quiet", action="store_true", help="display only answer")
+parser.add_argument("-a", "--author", action="store_true", help="print author")
+parser.add_argument("-d", "--date", action="store_true", help="print date")
 parser.add_argument("x", type=int, help="the base")
 parser.add_argument("y", type=int, help="the exponent")
 args = parser.parse_args()
@@ -20,3 +22,9 @@ elif args.verbose:
     print("{0} to the power {1} equals {2}".format(args.x, args.y, answer))
 else:
     print("{0}^{1} == {2}".format(args.x, args.y, answer))
+
+if args.author:
+    print("Author: Brian Moss")
+
+if args.date:
+    print("Date: Nov 18, 2014")
