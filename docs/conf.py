0ef9e4274636afc88df885535938f82962bda100
kallimachos <kallimachos@gmail.com>
2016-06-08 09:41:06 +1000

    Comment unused config

--- docs/conf.py
+++ docs/conf.py
@@ -120,7 +120,7 @@ pygments_style = 'sphinx'
 # keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+# todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
