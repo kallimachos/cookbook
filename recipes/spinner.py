aa2f2d7006d1fe305bfab98a4200fa8ae67c1925
kallimachos <kallimachos@gmail.com>
2019-07-25 11:24:49 +1000

    yaspin with logging

--- recipes/spinner.py
+++ recipes/spinner.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 """Example usage of the yaspin module."""
 
+import logging
 from time import sleep
 
 from yaspin import yaspin
 
 if __name__ == "__main__":
     with yaspin(text="Iterating", color="green") as spinner:
-        sleep(5)
+        sleep(2)
+        spinner.write("A message of great importance")
+        spinner.hide()
+        logging.error("hi, this is an error")
+        spinner.show()
+        spinner.text = "Still iterating"
+        sleep(2)
     spinner.ok()
