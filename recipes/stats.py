9af5284296df72aff68de0dbaa3ec9df833261aa
kallimachos <kallimachos@gmail.com>
2020-05-08 12:42:12 +1000

    Update stats.py

--- recipes/stats.py
+++ recipes/stats.py
@@ -19,17 +19,17 @@ def system_stats():
     """Get stats."""
     while True:
         stats = {}
-        stats["IP"] = psutil.net_if_addrs()["en0"][0].address
-        stats["CPU"] = psutil.cpu_percent(interval=1.0)
+        if platform.system() == "Darwin":
+            stats["IP"] = psutil.net_if_addrs()["en0"][0].address
+            stats["Battery"] = f"{psutil.sensors_battery().percent}%"
+        else:
+            stats["IP"] = psutil.net_if_addrs()["eth0"][0].address
+            stats["Temp"] = f"{psutil.sensors_temperatures()['cpu-thermal'][0].current} C"
+        stats["CPU"] = f"{psutil.cpu_percent(interval=1.0)}%"
         mem = psutil.virtual_memory()
         stats["Mem used"] = f"{bytes2human(mem.used)}/{bytes2human(mem.total)} - {mem.percent}%"
         disk = psutil.disk_usage("/")
         stats["Disk used"] = f"{bytes2human(disk.used)}/{bytes2human(disk.total)} - {disk.percent}%"
-        if hasattr(psutil, "sensors_temperatures"):
-            stats["Temp"] = psutil.sensors_temperatures()
-        if hasattr(psutil, "sensors_fans"):
-            stats["Fans"] = psutil.sensors_fans()
-        stats["Battery"] = f"{psutil.sensors_battery().percent}%"
         print_stats(stats)
     return
 
