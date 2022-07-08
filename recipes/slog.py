dd22df7695e371c71e3d85fc87681f4198f640a5
kallimachos <kallimachos@gmail.com>
2022-07-08 18:11:29 +1000

    slog

--- recipes/slog.py
+++ recipes/slog.py
@@ -1,34 +1,29 @@
-#!/bin/python3
-"""Basic example using structlog."""
+# #!/bin/python3
+"""Basic example using structlog.
 
-from logging import DEBUG, ERROR
+Log levels  -  https://docs.python.org/3/howto/logging.html#when-to-use-logging
+DEBUG    Detailed information, typically of interest only when diagnosing
+         problems.
+INFO 	   Confirmation that things are working as expected.
+WARNING  An indication that something unexpected happened, or indicative of
+         some problem in the near future (e.g. disk space low). The software
+         is still working as expected.
+ERROR    Due to a more serious problem, the software has not been able to
+         perform some function.
+CRITICAL A serious error, indicating that the program itself may be unable to
+         continue running.
+"""
+
+import logging
 
 import structlog
 
-# Log levels  -  https://docs.python.org/3/howto/logging.html#when-to-use-logging
-# DEBUG    Detailed information, typically of interest only when diagnosing
-#          problems.
-# INFO 	   Confirmation that things are working as expected.
-# WARNING  An indication that something unexpected happened, or indicative of
-#          some problem in the near future (e.g. disk space low). The software
-#          is still working as expected.
-# ERROR    Due to a more serious problem, the software has not been able to
-#          perform some function.
-# CRITICAL A serious error, indicating that the program itself may be unable to
-#          continue running.
-
-logger = structlog.get_logger()
+logger = structlog.getLogger()
 
 
-def logconfig(debug=False, console=False):
-    """Set logging configuration.
-
-    Args:
-        debug (bool, optional): True to set loglevel to DEBUG
-        console (bool, optional): True if logging to console.
-
-    """
-    loglevel = DEBUG
+def logconfig(console=False, debug=False):
+    """Run logconfig."""
+    loglevel = "DEBUG"
     processors = [structlog.processors.add_log_level]
     call_processor = [
         structlog.processors.CallsiteParameterAdder(
@@ -41,32 +36,121 @@ def logconfig(debug=False, console=False):
         ),
     ]
     if console and debug:
-        processors += call_processor + [structlog.dev.ConsoleRenderer()]
+        processors += call_processor
+        renderer = [structlog.dev.ConsoleRenderer()]
     elif console and not debug:
-        loglevel = ERROR
-        processors += [structlog.dev.ConsoleRenderer()]
+        loglevel = "ERROR"
+        renderer = [structlog.dev.ConsoleRenderer()]
     else:
         processors += [
             structlog.processors.TimeStamper(fmt="iso", utc=True),
             structlog.processors.format_exc_info,
-            structlog.processors.JSONRenderer(),
         ]
+        renderer = [structlog.processors.JSONRenderer()]
+
     structlog.configure(
-        wrapper_class=structlog.make_filtering_bound_logger(loglevel),
-        processors=processors,
+        processors=processors
+        + [
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
     )
-    logger.debug("This is a debug message")
-    logger.info("This is an info message")
-    logger.warning("This is a warning message")
-    logger.error("This is an error message")
-    logger.critical("This is a critical message")
-    try:
-        assert True is False
-    except AssertionError:
-        logger.exception("This is an exception message")
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        # These run ONLY on `logging` entries that do NOT originate within structlog.
+        foreign_pre_chain=[],
+        # These run on ALL entries after the pre_chain is done.
+        processors=processors
+        + [
+            # Remove _record & _from_structlog.
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+        ]
+        + renderer,
+    )
+
+    handler = logging.StreamHandler()
+    # Use OUR `ProcessorFormatter` to format all `logging` entries.
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(loglevel)
     return
 
 
-if __name__ == "__main__":
-    logconfig(debug=True, console=True)
-    logconfig(debug=True, console=False)
+# configure logging for imported modules
+# logfmt = "[%(log_color)s%(levelname)-9s] %(message)-20s filename=%(filename)s
+#           func_name=%(funcName)s lineno=%(lineno)d]"
+# datefmt = "%Y-%m-%d %H:%M:%S%z"
+# colorlog.basicConfig(format=logfmt, datefmt=datefmt, level=logging.DEBUG)
+
+# # configure logging for our modules
+# loglevel = logging.DEBUG
+# processors = [structlog.processors.add_log_level]
+# call_processor = [
+#     structlog.processors.CallsiteParameterAdder(
+#         parameters={
+#             structlog.processors.CallsiteParameter.FILENAME: "filename",
+#             structlog.processors.CallsiteParameter.MODULE: "module",
+#             structlog.processors.CallsiteParameter.FUNC_NAME: "func_name",
+#             structlog.processors.CallsiteParameter.LINENO: "lineno",
+#         }
+#     ),
+# ]
+# if console and debug:
+#     processors += call_processor + [structlog.dev.ConsoleRenderer()]
+# elif console and not debug:
+#     loglevel = logging.ERROR
+#     processors += [structlog.dev.ConsoleRenderer()]
+# else:
+#     processors += [
+#         structlog.processors.TimeStamper(fmt="iso", utc=True),
+#         structlog.processors.format_exc_info,
+#         structlog.processors.JSONRenderer(),
+#     ]
+# structlog.configure(
+#     wrapper_class=structlog.make_filtering_bound_logger(loglevel),
+#     processors=processors,
+#     cache_logger_on_first_use=True
+# )
+# logger = structlog.getLogger()
+# return logger
+
+# processors = [
+#     structlog.stdlib.add_log_level,
+#     structlog.processors.TimeStamper(fmt="iso", utc=True),
+#     structlog.processors.CallsiteParameterAdder(
+#         parameters={
+#             structlog.processors.CallsiteParameter.FILENAME: "filename",
+#             structlog.processors.CallsiteParameter.MODULE: "module",
+#             structlog.processors.CallsiteParameter.FUNC_NAME: "func_name",
+#             structlog.processors.CallsiteParameter.LINENO: "lineno",
+#         }
+#     ),
+# ]
+
+# structlog.configure(
+#     processors=processors + [
+#         structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+#     ],
+#     logger_factory=structlog.stdlib.LoggerFactory(),
+#     cache_logger_on_first_use=True,
+# )
+
+# formatter = structlog.stdlib.ProcessorFormatter(
+#     # These run ONLY on `logging` entries that do NOT originate within structlog.
+#     foreign_pre_chain=[],
+#     # These run on ALL entries after the pre_chain is done.
+#     processors=processors + [
+#         # Remove _record & _from_structlog.
+#         structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+#         structlog.dev.ConsoleRenderer(),
+#     ],
+# )
+
+# handler = logging.StreamHandler()
+# # Use OUR `ProcessorFormatter` to format all `logging` entries.
+# handler.setFormatter(formatter)
+# logger = logging.getLogger()
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
