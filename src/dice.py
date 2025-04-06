e03bfe4e9bf3ad83ac6b29c0db4350231d1401ea
kallimachos <kallimachos@gmail.com>
2025-04-06 19:55:44 +1000

    Streamline dice.py

--- src/dice.py
+++ src/dice.py
@@ -4,24 +4,13 @@ from random import randint
 
 
 def spread(low, high, reps):
-    above = 0
-    below = 0
-    for _x in range(reps):
-        num = randint(low, high)
-        if num > 10:
-            above += 1
-        else:
-            below += 1
-    return (above, below)
+    above = sum(1 for _ in range(reps) if randint(low, high) > 10)
+    below = reps - above
+    return above, below
 
 
 if __name__ == "__main__":
-    reps = 1000
-    # d20
-    low = 1
-    high = 20
-    d20 = spread(low, high, reps)
-    # d18
-    low = 3
-    high = 18
-    d18 = spread(low, high, reps)
+    d20 = spread(1, 20, 1000)
+    d18 = spread(3, 18, 1000)
+    print(f"d20: {d20[0]} above, {d20[1]} below")
+    print(f"d18: {d18[0]} above, {d18[1]} below")
